8491a47bb43e366bf4216be33b659c8ac29a77bd
Jon Derrick <jonathan.derrick@intel.com>
2015-06-17 09:49:19 -0600

    NVMe: Use CMB for the SQ if available
    Second revision. Remap the entire CMB and assign SQs into the CMB. Shrink the
    queues as needed. If the queue depth is shrunk below the threshold where the
    queue is no longer page-aligned, the CMB will not be used.
    
    Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -1361,7 +1361,8 @@ static void nvme_free_queue(struct nvme_queue *nvmeq)
 {
 	dma_free_coherent(nvmeq->q_dmadev, CQ_SIZE(nvmeq->q_depth),
 				(void *)nvmeq->cqes, nvmeq->cq_dma_addr);
-	dma_free_coherent(nvmeq->q_dmadev, SQ_SIZE(nvmeq->q_depth),
+	if (!nvmeq->dev->cmb)
+		dma_free_coherent(nvmeq->q_dmadev, SQ_SIZE(nvmeq->q_depth),
 					nvmeq->sq_cmds, nvmeq->sq_dma_addr);
 	kfree(nvmeq);
 }
@@ -1434,6 +1435,31 @@ static void nvme_disable_queue(struct nvme_dev *dev, int qid)
 	spin_unlock_irq(&nvmeq->q_lock);
 }
 
+static int nvme_alloc_queues_on_cmb(struct nvme_dev *dev, int nr_io_queues,
+					int q_size, int entry_size)
+{
+	int q_depth = dev->q_depth;
+	if (q_size * nr_io_queues > dev->cmb_size) {
+		q_depth = rounddown_pow_of_two(dev->cmb_size / nr_io_queues /
+						entry_size);
+		q_depth = min_t(int, q_depth, dev->q_depth);
+
+		/* To keep queues page-aligned */
+		if (q_depth < (dev->page_size / entry_size))
+			return -ENOMEM;
+	}
+
+	return q_depth;
+}
+
+static void *nvme_map_queue_on_cmb(struct nvme_dev *dev, int qid, int q_size,
+							dma_addr_t *q_dma_addr)
+{
+	unsigned offset = (qid - 1) * q_size;
+	*q_dma_addr = dev->cmb_dma_addr + offset;
+	return (void *)((uintptr_t)dev->cmb + offset);
+}
+
 static struct nvme_queue *nvme_alloc_queue(struct nvme_dev *dev, int qid,
 							int depth)
 {
@@ -1446,10 +1472,16 @@ static struct nvme_queue *nvme_alloc_queue(struct nvme_dev *dev, int qid,
 	if (!nvmeq->cqes)
 		goto free_nvmeq;
 
-	nvmeq->sq_cmds = dma_alloc_coherent(dev->dev, SQ_SIZE(depth),
+	if (qid && NVME_CMB_SQS(dev->cmbsz) && dev->cmb)
+		nvmeq->sq_cmds = nvme_map_queue_on_cmb(dev, qid,
+					SQ_SIZE(depth), &nvmeq->sq_dma_addr);
+	else {
+		nvmeq->sq_cmds = dma_alloc_coherent(dev->dev, SQ_SIZE(depth),
 					&nvmeq->sq_dma_addr, GFP_KERNEL);
-	if (!nvmeq->sq_cmds)
-		goto free_cqdma;
+
+		if (!nvmeq->sq_cmds)
+			goto free_cqdma;
+	}
 
 	nvmeq->q_dmadev = dev->dev;
 	nvmeq->dev = dev;
@@ -2132,6 +2164,39 @@ static int set_queue_count(struct nvme_dev *dev, int count)
 	return min(result & 0xffff, result >> 16) + 1;
 }
 
+static void __iomem *nvme_map_cmb(struct nvme_dev *dev,
+				dma_addr_t *cmb_dma_addr, u64 *cmb_size)
+{
+	u64 szu, size, bar_size, offset;
+	struct pci_dev *pdev = to_pci_dev(dev->dev);
+	void __iomem *cmb;
+	dma_addr_t dma_addr;
+
+	szu = 1 << (12 + 4 * NVME_CMB_SZU(dev->cmbsz));
+	size = szu * NVME_CMB_SZ(dev->cmbsz);
+	offset = szu * NVME_CMB_OFST(dev->cmbloc);
+	bar_size = pci_resource_len(pdev, NVME_CMB_BIR(dev->cmbloc));
+
+	if (size > bar_size - offset);
+		size = bar_size - offset;
+
+	dma_addr = pci_resource_start(pdev, NVME_CMB_BIR(dev->cmbloc)) + offset;
+	cmb = ioremap(dma_addr, size);
+	if (!cmb)
+		return NULL;
+
+	*cmb_dma_addr = dma_addr;
+	*cmb_size = size;
+	return cmb;
+
+}
+
+static inline void nvme_release_cmb(struct nvme_dev *dev)
+{
+	if (dev->cmb)
+		iounmap(dev->cmb);
+}
+
 static size_t db_bar_size(struct nvme_dev *dev, unsigned nr_io_queues)
 {
 	return 4096 + ((nr_io_queues + 1) * 8 * dev->db_stride);
@@ -2149,6 +2214,13 @@ static int nvme_setup_io_queues(struct nvme_dev *dev)
 		return result;
 	if (result < nr_io_queues)
 		nr_io_queues = result;
+
+	if (NVME_CMB_SQS(dev->cmbsz) && dev->cmb) {
+		result = nvme_alloc_queues_on_cmb(dev, nr_io_queues,
+			SQ_SIZE(dev->q_depth), sizeof(struct nvme_command));
+		if (result > 0)
+			dev->q_depth = result;
+	}
 
 	size = db_bar_size(dev, nr_io_queues);
 	if (size > 8192) {
@@ -2410,6 +2482,10 @@ static int nvme_dev_map(struct nvme_dev *dev)
 	dev->q_depth = min_t(int, NVME_CAP_MQES(cap) + 1, NVME_Q_DEPTH);
 	dev->db_stride = 1 << NVME_CAP_STRIDE(cap);
 	dev->dbs = ((void __iomem *)dev->bar) + 4096;
+
+	dev->cmbsz = readl(&dev->bar->cmbsz);
+	dev->cmbloc = readl(&dev->bar->cmbloc);
+	dev->cmb = nvme_map_cmb(dev, &dev->cmb_dma_addr, &dev->cmb_size);
 
 	return 0;
 
@@ -3108,6 +3184,7 @@ static void nvme_remove(struct pci_dev *pdev)
 	nvme_dev_remove_admin(dev);
 	device_destroy(nvme_class, MKDEV(nvme_char_major, dev->instance));
 	nvme_free_queues(dev, 0);
+	nvme_release_cmb(dev);
 	nvme_release_prp_pools(dev);
 	kref_put(&dev->kref, nvme_free_dev);
 }
