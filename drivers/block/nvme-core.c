ecaae76640a844f507711db30c8a211681f5cb17
Jon Derrick <jonathan.derrick@intel.com>
2015-06-23 11:37:48 -0600

    NVMe: Command submission path function return/check
    The current command submission path through nvme_submit_cmd and
    __nvme_submit_cmd has no points where they can fail to their callers.
    This patch cleans up the submission path return codes and the functions
    who call and check their return codes.
    
    Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -385,7 +385,8 @@ static inline void nvme_put_cmd(struct nvme_queue *nvmeq,
  *
  * Safe to use from interrupt context
  */
-static int __nvme_submit_cmd(struct nvme_queue *nvmeq, struct nvme_command *cmd)
+static void __nvme_submit_cmd(struct nvme_queue *nvmeq,
+				struct nvme_command *cmd)
 {
 	u16 tail = nvmeq->sq_tail;
 
@@ -394,18 +395,14 @@ static int __nvme_submit_cmd(struct nvme_queue *nvmeq, struct nvme_command *cmd)
 		tail = 0;
 	writel(tail, nvmeq->q_db);
 	nvmeq->sq_tail = tail;
-
-	return 0;
 }
 
-static int nvme_submit_cmd(struct nvme_queue *nvmeq, struct nvme_command *cmd)
+static void nvme_submit_cmd(struct nvme_queue *nvmeq, struct nvme_command *cmd)
 {
 	unsigned long flags;
-	int ret;
 	spin_lock_irqsave(&nvmeq->q_lock, flags);
-	ret = __nvme_submit_cmd(nvmeq, cmd);
+	__nvme_submit_cmd(nvmeq, cmd);
 	spin_unlock_irqrestore(&nvmeq->q_lock, flags);
-	return ret;
 }
 
 static __le64 **iod_list(struct nvme_iod *iod)
@@ -785,7 +782,7 @@ static void nvme_submit_flush(struct nvme_queue *nvmeq, struct nvme_ns *ns,
 	__nvme_submit_cmd(nvmeq, &cmnd);
 }
 
-static int nvme_submit_iod(struct nvme_queue *nvmeq, struct nvme_iod *iod,
+static void nvme_submit_iod(struct nvme_queue *nvmeq, struct nvme_iod *iod,
 							struct nvme_ns *ns)
 {
 	struct request *req = iod_get_private(iod);
@@ -831,8 +828,6 @@ static int nvme_submit_iod(struct nvme_queue *nvmeq, struct nvme_iod *iod,
 	cmnd.rw.dsmgmt = cpu_to_le32(dsmgmt);
 
 	__nvme_submit_cmd(nvmeq, &cmnd);
-
-	return 0;
 }
 
 /*
@@ -1073,7 +1068,9 @@ static int nvme_submit_async_admin_req(struct nvme_dev *dev)
 	c.common.command_id = req->tag;
 
 	blk_mq_free_request(req);
-	return __nvme_submit_cmd(nvmeq, &c);
+	__nvme_submit_cmd(nvmeq, &c);
+
+	return 0;
 }
 
 static int nvme_submit_admin_async_cmd(struct nvme_dev *dev,
@@ -1095,8 +1092,9 @@ static int nvme_submit_admin_async_cmd(struct nvme_dev *dev,
 	cmdinfo->status = -EINTR;
 
 	cmd->common.command_id = req->tag;
+	nvme_submit_cmd(nvmeq, cmd);
 
-	return nvme_submit_cmd(nvmeq, cmd);
+	return 0;
 }
 
 static int adapter_delete_queue(struct nvme_dev *dev, u8 opcode, u16 id)
@@ -1306,12 +1304,7 @@ static void nvme_abort_req(struct request *req)
 
 	dev_warn(nvmeq->q_dmadev, "Aborting I/O %d QID %d\n", req->tag,
 							nvmeq->qid);
-	if (nvme_submit_cmd(dev->queues[0], &cmd) < 0) {
-		dev_warn(nvmeq->q_dmadev,
-				"Could not abort I/O %d QID %d",
-				req->tag, nvmeq->qid);
-		blk_mq_free_request(abort_req);
-	}
+	nvme_submit_cmd(dev->queues[0], &cmd);
 }
 
 static void nvme_cancel_queue_ios(struct request *req, void *data, bool reserved)
